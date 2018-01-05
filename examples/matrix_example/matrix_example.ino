11192e4a3dc5f5b97245b68fea8c61ff06eaf7e8
Earsuit <renyudicn@outlook.com>
2018-01-05 16:25:48 +0800

    Update matrix_example.ino

--- examples/matrix_example/matrix_example.ino
+++ examples/matrix_example/matrix_example.ino
@@ -5,6 +5,7 @@ void setup() {
     float array1[4][4] = {{16,19,2,7},{5,34,77,1},{23,37,38,7},{9,33,10,0}};
     float array2[4][1] = {{5},{6},{7},{8}};
     //create float type matrices
+    //You could replace the "float" by the other type you want
     Matrix<float> A(4,4,(float*)array1);
     Matrix<float> B(4,1,(float*)array2);
     Matrix<float> C = A*B;
