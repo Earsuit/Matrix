58b9635e3d48c43d1567c20373fa70ee030ddb19
Earsuit <renyudicn@outlook.com>
2018-01-05 17:43:54 +0800

    Update matrix_example.ino

--- examples/matrix_example/matrix_example.ino
+++ examples/matrix_example/matrix_example.ino
@@ -5,16 +5,16 @@ void setup() {
     float array1[4][4] = {{16,19,2,7},{5,34,77,1},{23,37,38,7},{9,33,10,0}};
     float array2[4][1] = {{5},{6},{7},{8}};
     //create float type matrices
-    //You could replace the "float" by the other type you want
     Matrix<float> A(4,4,(float*)array1);
     Matrix<float> B(4,1,(float*)array2);
     Matrix<float> C = A*B;
-    //create identity matrix
-    Matrix<float> D(4,4,'I');
+    Matrix<float> D(4,4,'I');  //create identity matrix
     Serial.println("C: ");
     C.show();
+    Serial.println("A transpose: ");
+    Matrix<float>::transpose(A).show(4);
     Serial.println("A inverse: ");
-    A.inv().show(4);
+    Matrix<float>::inv(A).show(4);
     Serial.println("D: ");
     D.show();
 }
