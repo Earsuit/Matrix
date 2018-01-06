83a5bcffc6b4bfc7bc3932889bbe6a08af7914e1
Earsuit <renyudicn@outlook.com>
2018-01-06 11:20:57 +0800

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -8,8 +8,7 @@
   One should use the inverse function inv() instead.
 
   To get a better performance, it's recommended to use /=, *=, -= and += if you
-  have A = A*B, A = A+B or A = A/b (b is a scalar). If you have A = B*C,
-  the A.times(const Matrix & B,const Matrix & C) function gives faster performance.
+  have A = A*B, A = A+B or A = A/b (b is a scalar).
 
   However, using this library to do matrix calculation is not very
   efficient, if you have a time-critical sketch, it would be better to write your
